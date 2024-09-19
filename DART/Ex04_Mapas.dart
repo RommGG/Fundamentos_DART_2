8e758934aa7b18d26032d287456837bbb1b32dd5
Romualdo <210714@utxicotepec.edu.mx>
2024-09-19 11:40:49 -0600

    Practica 06
    Se han mandado cambios a revision

--- DART/Ex04_Mapas.dart
+++ DART/Ex04_Mapas.dart
@@ -1,6 +1,6 @@
 void main() {
   final pokemon = {
-    "name": "Pikachu",
+    "Name": "Pikachu",
     "HP": 100,
     "isAlive" : true,
     "abilities": ['impact Trueno','Cola de Hierro','Rapido'],
@@ -18,9 +18,50 @@ void main() {
     'isGymnasiumLeader': false
     };
   
+  final pokemons = {
+    1:"Pikachu",
+    2: "Charmander",
+    3: "Squirtle",
+    4: "Pidgeot"
+  };
+  
   
   
   //Imprimir los datos de un mapa
-  print("""Los datos actuales del mapa son:
-  $pokemon""");
+  print("""Los datos del Pokemon son usando el Mapa son:
+  ----------------------------------------------------------
+  Pokemon =$pokemon
+  Trainer= $trainer
+  Pokemons = $pokemons""");
+  print("**********************************************************************************************");
+  print("""
+  Accediendo a las propiedades del pokemon usando los braquets/corchetes []:
+  ======================================================================
+  Nombre = ${pokemon['Name']}
+  HP = ${pokemon['HP']}
+  Vivo = ${pokemon['isAlive']}
+  Sprites = ${pokemon['sprites']}
+  ======================================================================""");
+  
+ 
+  //Reto 01:Obtener el valor indeoendiente de las imagenes de Fron y Back del Pokemon
+  //front: ${(pokemon['saprites'])[1]}
+  //back: ${pokemon['sprites'][2]}
+  
+  //Dado que la solucion ya esta obsoleta lo que queda es crear una nueva variable con el objeto y destructura las imagenes para acceder a ellas 
+  
+  final tmp_sprites = pokemon['sprites'] as Map<int,String>;
+  
+  print(""" 
+   Accediendo a las propiedades del pokemon usando los braquets/corchetes []:
+  ======================================================================
+  Nombre = ${pokemon['Name']}
+  HP = ${pokemon['HP']}
+  Vivo = ${pokemon['isAlive']}
+  Sprites = ${pokemon['sprites']}
+  -----------------------------------------
+  Back: ${tmp_sprites[2]}
+  Front: ${tmp_sprites[1]}
+  ======================================================================
+  """);
 }
